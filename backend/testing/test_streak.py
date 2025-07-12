bce8754513c057b89c1d0414cae38dbb425744c3
TheVex <66883116+TheVex@users.noreply.github.com>
2025-07-12 22:14:45 +0300

    fix: streaks and test for them

--- backend/testing/test_streak.py
+++ backend/testing/test_streak.py
@@ -10,11 +10,17 @@ def test_check_in():
         "Content-Type": "application/json"
     }
 
-    url = f"http://localhost:{PORT}/me/check_in"
+    check_in_url = f"http://localhost:{PORT}/me/check_in"
+    get_streaks_url = f"http://localhost:{PORT}/me/streaks"
 
     data = {
         "user_id": "00000000-0000-0000-0000-000000000001"
     }
-    check_in_response = requests.post(url, headers=header, json=data)
-
+    get_streaks_response_before = requests.post(get_streaks_url, headers=header, json=data)
+    print(get_streaks_response_before.json())
+    check_in_response = requests.post(check_in_url, headers=header, json=data)
+    print(check_in_response.json())
+    get_streaks_response_after = requests.post(get_streaks_url, headers=header, json=data)
+    print(get_streaks_response_after.json())
     assert check_in_response.status_code == 200
+    assert get_streaks_response_after.json()["data"][0] is not None
