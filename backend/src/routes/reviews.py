b6911274db2a8e9626f55e619f7e4e3a7a33ba43
TheVex <66883116+TheVex@users.noreply.github.com>
2025-07-13 15:38:39 +0300

    add: new method

--- backend/src/routes/reviews.py
+++ backend/src/routes/reviews.py
@@ -34,6 +34,37 @@ async def get_reviews(request: UserRequest):
     }
 
 
+@router.get("/me/reviews/{book_id}/all_reviews")
+async def get_reviews_for_book(book_id: UUID):
+    data, err = db_handler.getReview(book_id=book_id)
+    answer = []
+    if err:
+        if isinstance(err, ValueError):
+            raise HTTPException(status_code=404, detail={
+                "status": "error",
+                "message": "Book not found."
+            })
+        else:
+            raise HTTPException(status_code=400, detail={
+                "status": "error",
+                "message": str(err)
+            })
+    if data:
+        for review in data:
+            answer.append(ReviewData(
+                rate=review.rate,
+                text=review.text,
+                book_id=review.book_id,
+                user_id=review.user_id,
+                created_at=review.created_at
+            ))
+    return {
+        "status": "success",
+        "message": "Reviews retrieved",
+        "data": answer
+    }
+
+
 @router.post("/me/reviews/{book_id}", response_model=ReviewResponse, status_code=200)
 async def get_review(request: UserRequest, book_id: UUID):
     data, err = db_handler.getReview(user_id=request.user_id, book_id=book_id)
